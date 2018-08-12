d6ef10bb78fbdd872c0f741856603826b8ed7557
unknown <jim88star@gmail.com>
2018-08-11 21:25:18 -0500

    User API routes and JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jim:jim123@ds127429.mlab.com:27429/devconnector'
+  mongoURI: 'mongodb://jim:jim123@ds127429.mlab.com:27429/devconnector',
+  secretOrKey: 'secret'
 };
